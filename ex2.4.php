1ce6f20cee5f7ccb8b180d4c9c37a563c5ad9428
erdeks <aponcelas95@gmail.com>
2016-09-23 20:20:43 +0200

    calendario

---
+++ ex2.4.php
@@ -0,0 +1,22 @@
+<?php
+$i = 12;
+$x = 7.7;
+$y = false;
+$z = "Kindred";
+$tipus_de_i = gettype ($i);
+$tipus_de_x = gettype ($x);
+$tipus_de_y = gettype ($y);
+$tipus_de_z = gettype ($z);
+echo "La variable \$i
+	conté el valor $i
+	i és del tipus $tipus_de_i</br>";
+echo "La variable \$x
+	conté el valor $x
+	i és del tipus $tipus_de_x</br>";
+echo "La variable \$y
+	conté el valor $y
+	i és del tipus $tipus_de_y</br>";
+echo "La variable \$z
+	conté el valor $z
+	i és del tipus $tipus_de_z</br>;
+?>
